1a9b69ae02d68a317f7320938467bd619d41f228
0r3s7 <orestemirra03@gmail.com>
2025-04-18 11:52:22 +0200

    New File added

---
+++ py4github.py
@@ -0,0 +1,12 @@
+
+# This is for testing Github Flow uploading a simple *.py file
+
+a=0
+b=0
+
+while (a<=10):
+    print(f"While a is {a}, b equals {b}")
+    b+=1
+    a+=1
+
+print("\nCycle terminated!")
